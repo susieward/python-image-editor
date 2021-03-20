c7a80d49187b38808be182ab011633314906cfcf
Susie Ward <susie@susieward.dev>
2021-03-20 17:08:04 -0500

    build script edit

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo systemctl stop python-image-editor
+sudo -S systemctl stop python-image-editor
 cd ~/python-image-editor
+unset GIT_DIR
 git pull
 source env/bin/activate
 pip3 install -r requirements.txt
 deactivate
 cd ~/
-sudo systemctl start python-image-editor
+sudo -S systemctl start python-image-editor
